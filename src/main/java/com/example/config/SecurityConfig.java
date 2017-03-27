3e61f2ab9c68755480d0c05d66ef80e67d0c255e
java <java>
2017-03-27 10:24:23 +0900

    login

---
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		/*
+		 * 인증
+		 */
+		auth.inMemoryAuthentication()
+			.withUser("admin").password("1234").roles("ADMIN")
+			.and()
+			.withUser("user").password("1234").roles("USER");
+	}
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		/*
+		 * 권한
+		 */
+		http.authorizeRequests()
+			.antMatchers("/country/**").hasRole("ADMIN")
+//			.antMatchers("/city/**").hasRole("ADMIN")
+			.and()
+			.formLogin()
+			.permitAll();
+			
+		
+		
+	}
+
+}
